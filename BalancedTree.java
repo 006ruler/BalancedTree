eb471541bf2daed9d6bbb8b333e01f694b123695
Nicus Hicks <nicus.hicks@gmail.com>
2019-01-02 02:38:32 -0600

    A little more work.
    I need to really look into building trees correctly, currently my blind attempts have me worried.

--- BalancedTree.java
+++ BalancedTree.java
@@ -8,6 +8,23 @@ public class BalancedTree {
     public BalancedTree(Object data) {
         root = new Node(null, null, null, (Comparable) data);
     }
+
+    public void addNode(Object data) {
+        Node tempNode = this.root;
+        Node newNode = new Node(null, null, null, (Comparable) data);
+        while(newNode.getParent() == null) {
+            if(tempNode.oompareTo(newNode) < 0) {
+                if(tempNode.getLeft() == null) {
+                    tempNode.setLeft(newNode);
+                    newNode.setParent(tempNode);
+                }
+                else {
+                    tempNode = tempNode.getLeft();
+                }
+            }
+            //TODO equal to, and greather than
+        }
+    }
 }
 
 class Node<A extends Comparable<? super A>> {
@@ -32,8 +49,23 @@ class Node<A extends Comparable<? super A>> {
         return this.data.compareTo((A) otherNode.getData());
     }
 
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
     public A getData() {
         return this.data;
     }
+    public Node getParent() { return this.parent;}
+    public Node getLeft() { return this.left;}
+    public Node getRight() { return this.right;}
 
 }
